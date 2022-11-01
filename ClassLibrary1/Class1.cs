0a48d54c2ad5ff107dce40fbfa1173910483c967
Zidooo <xalessssj@gmail.com>
2022-11-01 22:20:31 +0300

    Добавьте файлы проекта.

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        // Объявляем делегат
+        public delegate void AccountHandler(string message);
+        public class Account
+        {
+            int sum;
+            // Создаем переменную делегата
+            AccountHandler taken;
+            public Account(int sum)
+            {
+                this.sum = sum;
+            }
+
+            // Регистрируем делегат
+            public void RegisterHandler(AccountHandler del)
+            {
+                taken = del;
+            }
+            public void Add(int sum)
+            {
+                this.sum += sum;
+                taken?.Invoke($"К счету добалвено {sum} Баланс: {this.sum} у.е.");
+            }
+
+
+            public void Take(int sum)
+            {
+                if (this.sum >= sum)
+                {
+                    this.sum -= sum;
+                    // вызываем делегат, передавая ему сообщение
+                    taken?.Invoke($"Со счета списано {sum} у.е. Баланс: {this.sum}");
+                }
+                else
+                {
+                    taken?.Invoke($"Недостаточно средств. Баланс: {this.sum} у.е.");
+                }
+            }
+        }
+    }
+}
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static ClassLibrary1.Class1;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public string sum { get; }
+        public string fio { get; }
+        auth iniz;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public class auth
+        {
+            public string fio;
+            public int sum;
+
+            public auth(string FIO, int SUM)
+            {
+                fio = FIO;
+                sum = SUM;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // создаем банковский счет
+            Account account = new Account(Convert.ToInt32(textBox2.Text));
+            // Добавляем в делегат ссылку на метод PrintSimpleMessage
+            account.RegisterHandler(PrintSimpleMessage);
+            // Два раза подряд пытаемся снять деньги
+            account.Take(Convert.ToInt32(textBox3.Text));
+            void PrintSimpleMessage(string message) => listBox1.Items.Add(message);
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // создаем банковский счет
+            Account account = new Account(Convert.ToInt32(textBox2.Text));
+            // Добавляем в делегат ссылку на метод PrintSimpleMessage
+            account.RegisterHandler(PrintSimpleMessage);
+            // Два раза подряд пытаемся снять деньги
+            account.Add(Convert.ToInt32(textBox3.Text));
+            void PrintSimpleMessage(string message) => listBox1.Items.Add(message);
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            iniz = new auth(fio, Convert.ToInt32(sum));
+        }
+    }
+}
